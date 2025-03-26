068d5791a3fa621e8a4b797e055fb02ea8b1e1ee
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2025-03-26 13:49:26 -0300

    update

--- assets/script/index.js
+++ assets/script/index.js
@@ -44,3 +44,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 60, // Ajuste para compensar a navbar fixa
+                behavior: 'smooth'
+            });
+        }
+    });
+});
