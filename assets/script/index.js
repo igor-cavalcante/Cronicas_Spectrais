76761b24efe5380a534e7bf264c33846dd8b42c2
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2025-06-17 17:19:00 -0300

    Finalizing the Events & Launch Page

--- assets/script/index.js
+++ assets/script/index.js
@@ -44,37 +44,38 @@ navItems.forEach(link => {
 
 
 // button dropdownBtn
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownBtn = document.getElementById("dropdownBtn");
-    const dropdownMenu = document.getElementById("dropdownMenu");
 
-    dropdownBtn.addEventListener("click", function () {
-        dropdownMenu.classList.toggle("opacity-0");
-        dropdownMenu.classList.toggle("invisible");
-        dropdownMenu.classList.toggle("scale-95");
-    });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const dropdownBtn = document.getElementById("dropdownBtn");
+//     const dropdownMenu = document.getElementById("dropdownMenu");
 
-    // Fechar menu se clicar fora dele
-    document.addEventListener("click", function (event) {
-        if (!dropdownBtn.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.classList.add("opacity-0", "invisible", "scale-95");
-        }
-    });
-});
+//     dropdownBtn.addEventListener("click", function () {
+//         dropdownMenu.classList.toggle("opacity-0");
+//         dropdownMenu.classList.toggle("invisible");
+//         dropdownMenu.classList.toggle("scale-95");
+//     });
+
+//     // Fechar menu se clicar fora dele
+//     document.addEventListener("click", function (event) {
+//         if (!dropdownBtn.contains(event.target) && !dropdownMenu.contains(event.target)) {
+//             dropdownMenu.classList.add("opacity-0", "invisible", "scale-95");
+//         }
+//     });
+// });
 
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            window.scrollTo({
-                top: target.offsetTop - 60, // Ajuste para compensar a navbar fixa
-                behavior: 'smooth'
-            });
-        }
-    });
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const target = document.querySelector(this.getAttribute('href'));
+//         if (target) {
+//             window.scrollTo({
+//                 top: target.offsetTop - 60, // Ajuste para compensar a navbar fixa
+//                 behavior: 'smooth'
+//             });
+//         }
+//     });
+// });
 
 
 
